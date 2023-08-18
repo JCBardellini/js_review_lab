d413e0af415a39b085cde49d8c64e54ce071fe80
Jean Bardellini <jbardellini@LABA.PSWAS.LOCAL>
2023-08-18 09:38:01 -0700

    the farm - 4

---
+++ index.js
@@ -0,0 +1,44 @@
+/* A. Q + A
+1 - = (we can assign a variable with an assignment operator ex: let variableName = "something")
+2 - = (we can change the value of the variable with the assignment operator ex: variableName = "newValue")
+3 - = (we use the assignment operator to assign the existing variable to new variable ex: let newVariable = oldVariable)
+4 - declare - a variable with no value, assign - assigning a value to the declare variable , define - it is the value of the variable or function or object
+5 - pseudocoding is writing out the steps to solve the problem logically without writing the code
+6 - We should spend around 90% thinking about solving the problem before writing code
+*/
+
+//  B. Strings
+//1
+let firstVariable; // we are declaring the variable
+//2
+firstVariable = "Hello World"; // assigning the value
+//3
+firstVariable = 2; // changing the value
+//4
+let secondVariable = firstVariable; // declaring old var and setting value to new var
+//5
+secondVariable = "new string"; // changing the value
+//6:  firstVariable is "new string"
+
+//7
+let yourName = "JC"; // setting a variable with a value
+console.log("hello, my name is ", yourName);
+
+// C. Booleans
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = "kevin";
+
+console.log(a < b);
+console.log(c > d);
+console.log("kevin" === "kevin");
+console.log(true || false);
+console.log(false || false || false || false || false || true);
+console.log(false == false);
+console.log(e == "kevin");
+console.log(a + b == c);
+console.log(a * a == d);
+console.log(48 == "48");
